87af79cda611fae9eb641f9a4e699e6a0cc5f0c4
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-02-06 13:45:39 -0500

    Add button to register new expense

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
 import styles from "./styles";
 
 export default function Home() {
+  const handleAddExpense = () => {
+    console.log("Add expense")
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.floatingButton} onPress={handleAddExpense}>
+        <AntDesign name="plus" size={30} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,7 +1,31 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    width: 65,
+    height: 65,
+    borderRadius: "50%",
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
 });
 
 export default styles;

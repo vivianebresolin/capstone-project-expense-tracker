1bc13160984d23072d05b966712b895ee0e183d2
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-02-06 14:24:46 -0500

    Add modal in the Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,35 @@
-import { View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { View, TouchableOpacity, Modal, Text, Pressable } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
 import styles from "./styles";
 
 export default function Home() {
-  const handleAddExpense = () => {
-    console.log("Add expense")
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handlePressAddExpense = () => {
+    setModalVisible(true);
   }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.floatingButton} onPress={handleAddExpense}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <TouchableOpacity style={styles.floatingButton} onPress={handlePressAddExpense}>
         <AntDesign name="plus" size={30} color="black" />
       </TouchableOpacity>
     </View>

f807a6f5dbf23e28247a742e6f69232204622fbb
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-02-06 13:26:22 -0500

    Add tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, Ionicons } from '@expo/vector-icons';
+import Home from './src/screens/Home';
+import Insights from './src/screens/Insights';
+import Settings from './src/screens/Settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Expense Tracker</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => {
+              return (
+                <Entypo name="home" size={24} color={color} />
+              );
+            }
+          }}
+        />
+        <Tab.Screen
+          name="Insights"
+          component={Insights}
+          options={{
+            title: 'Insights',
+            tabBarIcon: ({ color }) => {
+              return (
+                <Ionicons name="stats-chart" size={24} color={color} />
+              );
+            }
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            title: 'Settings',
+            tabBarIcon: ({ color }) => {
+              return (
+                <Ionicons name="settings-sharp" size={24} color={color} />
+              );
+            }
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}

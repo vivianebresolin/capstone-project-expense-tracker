fa6ee67a0f0c2eb3f30737f168e1948510c381ac
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-02-06 22:21:56 -0500

    Add splash screen

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
+import { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Entypo, Ionicons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
 import Home from './src/screens/Home';
 import Insights from './src/screens/Insights';
 import Settings from './src/screens/Settings';
 
+SplashScreen.preventAutoHideAsync();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [isDataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataLoaded(true)
+    }, 4000)
+    SplashScreen.hideAsync();
+  }, []);
+
+  if (!isDataLoaded) {
+    return (
+      <View style={styles.loadingIndicatorContainer}>
+        <ActivityIndicator size="large" color="#0076FF" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
@@ -51,3 +73,15 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  loadingIndicatorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

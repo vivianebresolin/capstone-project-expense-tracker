b43b4205e1b14dfed58c09bf18a9d7ad668d7b25
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-02-07 01:51:19 -0500

    Add useExpenses context

--- App.js
+++ App.js
@@ -1,87 +1,56 @@
-import { useState, useEffect } from 'react';
-import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Entypo, Ionicons } from '@expo/vector-icons';
-import * as SplashScreen from 'expo-splash-screen';
 import Home from './src/screens/Home';
 import Insights from './src/screens/Insights';
 import Settings from './src/screens/Settings';
+import { ExpensesProvider } from './src/context/expensesContext';
 
-SplashScreen.preventAutoHideAsync();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [expenses, setExpenses] = useState([]);
-  const [isDataLoaded, setDataLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setDataLoaded(true)
-    }, 4000)
-    SplashScreen.hideAsync();
-  }, []);
-
-  if (!isDataLoaded) {
-    return (
-      <View style={styles.loadingIndicatorContainer}>
-        <ActivityIndicator size="large" color="#0076FF" />
-      </View>
-    );
-  }
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color }) => {
-              return (
-                <Entypo name="home" size={24} color={color} />
-              );
-            }
-          }}
-        />
-        <Tab.Screen
-          name="Insights"
-          component={Insights}
-          options={{
-            title: 'Insights',
-            tabBarIcon: ({ color }) => {
-              return (
-                <Ionicons name="stats-chart" size={24} color={color} />
-              );
-            }
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            title: 'Settings',
-            tabBarIcon: ({ color }) => {
-              return (
-                <Ionicons name="settings-sharp" size={24} color={color} />
-              );
-            }
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ExpensesProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Home',
+              tabBarIcon: ({ color }) => {
+                return (
+                  <Entypo name="home" size={24} color={color} />
+                );
+              }
+            }}
+          />
+          <Tab.Screen
+            name="Insights"
+            component={Insights}
+            options={{
+              title: 'Insights',
+              tabBarIcon: ({ color }) => {
+                return (
+                  <Ionicons name="stats-chart" size={24} color={color} />
+                );
+              }
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              title: 'Settings',
+              tabBarIcon: ({ color }) => {
+                return (
+                  <Ionicons name="settings-sharp" size={24} color={color} />
+                );
+              }
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ExpensesProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  loadingIndicatorContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
--- src/database/read.js
+++ src/database/read.js
@@ -1,8 +1,10 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './config';
+import { getUniqueId } from '../utils/deviceInfo';
 
 export async function getAllExpenses() {
-  const querySnapshot = await getDocs(collection(db, 'expenses'));
+  const deviceId = await getUniqueId();
+  const querySnapshot = await getDocs(collection(db, 'users', deviceId, 'expenses'));
   const expensesList = [];
 
   querySnapshot.forEach((doc) => {

00088f5e72848120136b6a6977394ef49620788c
Kristina Papko <papko.kristina.qa.com>
2024-03-06 19:10:53 -0500

    Added Settings Screen

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,10 +1,160 @@
-import { Text, View } from "react-native";
 import styles from "./styles";
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+  Switch,
+  Image,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 export default function Settings() {
+  const [form, setForm] = useState({
+    darkMode: false,
+    emailNotifications: true,
+    pushNotifications: false,
+  });
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.container}>
+        <View style={styles.profile}>
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <View style={styles.profileAvatarWrapper}>
+              <Image
+                alt=""
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                }}
+                style={styles.profileAvatar} />
+
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}>
+                <View style={styles.profileAction}>
+                  <FeatherIcon
+                    color="#fff"
+                    name="edit-3"
+                    size={15} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+          <View>
+            <Text style={styles.profileName}>John Doe</Text>
+            <Text style={styles.profileAddress}>
+              123 Maple Street. London, ON N6U 6T5
+            </Text>
+          </View>
+        </View>
+
+        <ScrollView>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Account Settings</Text>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.row}>
+              <Text style={styles.rowLabel}>Edit Profile</Text>
+              <View style={styles.rowSpacer} />
+              <FeatherIcon
+                color="#C6C6C6"
+                name="chevron-right"
+                size={20} />
+            </TouchableOpacity>
+            <View style={styles.row}>
+              <Text style={styles.rowLabel}>Dark Mode</Text>
+              <View style={styles.rowSpacer} />
+              <Switch
+                onValueChange={darkMode => setForm({ ...form, darkMode })}
+                value={form.darkMode} />
+            </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.row}>
+              <Text style={styles.rowLabel}>Change Password</Text>
+
+              <View style={styles.rowSpacer} />
+
+              <FeatherIcon
+                color="#C6C6C6"
+                name="chevron-right"
+                size={20} />
+            </TouchableOpacity>
+
+            <View style={styles.row}>
+              <Text style={styles.rowLabel}>Email Notifications</Text>
+              <View style={styles.rowSpacer} />
+              <Switch
+                onValueChange={emailNotifications =>
+                  setForm({ ...form, emailNotifications })
+                }
+                value={form.emailNotifications} />
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.rowLabel}>Push Notifications</Text>
+              <View style={styles.rowSpacer} />
+              <Switch
+                onValueChange={pushNotifications =>
+                  setForm({ ...form, pushNotifications })
+                }
+                value={form.pushNotifications} />
+            </View>
+          </View>
+
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>More</Text>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.row}>
+              <Text style={styles.rowLabel}>About Us</Text>
+              <View style={styles.rowSpacer} />
+              <FeatherIcon
+                color="#C6C6C6"
+                name="chevron-right"
+                size={20} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.row}>
+              <Text style={styles.rowLabel}>Contact Us</Text>
+              <View style={styles.rowSpacer} />
+              <FeatherIcon
+                color="#C6C6C6"
+                name="chevron-right"
+                size={20} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.row}>
+              <Text style={styles.rowLabel}>Privacy Policy</Text>
+              <View style={styles.rowSpacer} />
+              <FeatherIcon
+                color="#C6C6C6"
+                name="chevron-right"
+                size={20} />
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }

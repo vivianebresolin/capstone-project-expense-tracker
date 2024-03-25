b315e207b175c380f68c5f29750ca5f87b8dec4e
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2024-03-24 22:01:14 -0400

    Add bar chart

--- src/screens/Insights/index.js
+++ src/screens/Insights/index.js
@@ -1,15 +1,16 @@
-import { ScrollView, Text, View } from "react-native";
-import styles from "./styles";
+import { ScrollView, View } from "react-native";
 import AppPieChart from "./components/AppPieChart";
+import AppBarChart from "./components/AppBarChart";
+import styles from "./styles";
 
 export default function Insights() {
-
   return (
     <ScrollView
-      style={{
-        flex: 1,
-      }}>
-      <AppPieChart />
+      style={styles.scrollView}>
+      <AppBarChart />
+      <View style={styles.pieChartView}>
+        <AppPieChart />
+      </View>
     </ScrollView>
   );
 }
--- src/screens/Insights/styles.js
+++ src/screens/Insights/styles.js
@@ -1,7 +1,13 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+  scrollView: {
+    flex: 1,
+    padding: 20
+  },
+  pieChartView: {
+    marginVertical: 35
+  }
 });
 
 export default styles;
